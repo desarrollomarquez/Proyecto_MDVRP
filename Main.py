4c4e3fd37bbc12cd19e8c4c84531646c3bed8458
Diego Márquez <desarrollo.marquez@gmail.com>
2019-07-24 06:47:08 -0500

    Added Refactoring
    Added Refactoring

--- Main.py
+++ Main.py
@@ -4,70 +4,49 @@ import pyomo
 from pyomo.environ import *
 from Utils import *
 
-# Parametros:
 
-# N Número de vehículos
-# Cij Costos entre los nodos i y j
-# Wi Capacidad del depósito
-# Di Demanda del cliente j
-# Qk Capacidad del vehículo (ruta) k
-# l cardinalidad de los depositos.
-# m cardinalidad del conjunto de vehiculos.
-# n cardinalidad del conjunto de nodos.
-
-d = []
-[d.append(p) for p in range(1, len(data_depositos)+1)]
-c = []
-[c.append(p) for p in range(1, len(data_clientes)+1)]
-v = []
-[v.append(p) for p in range(1, len(data_vehiculos)+1)]
-
-# Capacidad del depósito (Wi)
-W = []
-[W.append(p) for p in capacity_d]
-# Demanda del cliente (Dj)
-D = []
-[D.append(p) for p in demanda]
-# Capacidad del Vehiculo (Qk)
-Q = []
-[Q.append(p) for p in capacity_v]
-
-l = len(d)
-n = len(c) 
-m = len(v)
-
-
-print("------ tamaño vectores ----------")
-print(l,"-> Depositos")
-print("\n")
-print(n,"-> Clientes")
-print("\n") 
-print(m,"-> Vehiculos")
-print("\n")
 
 # Dominio del Modelo - MDVRP
+
+modelo = ConcreteModel()
+
+
+
+# Parametros:
 
 # I Conjunto de depósitos
 # J Conjunto de clientes
 # K Conjunto de vehículos
 
-modelo = ConcreteModel()
+# N Número de vehículos
+# Cij Costos entre los nodos i y j
+# l cardinalidad de los depositos.
+# m cardinalidad del conjunto de vehiculos.
+# n cardinalidad del conjunto de nodos.
+
 
 modelo.i = Set(initialize=d, doc='Deposito')
 modelo.j = Set(initialize=c, doc='Cliente')
 modelo.k = Set(initialize=v, doc='Vehiculo')
 
+# Wi Capacidad del depósito
+# Di Demanda del cliente j
+# Qk Capacidad del vehículo (ruta) k
+
+modelo.w = Var(W)
+modelo.d = Var(D)
+modelo.q = Param(modelo.k, initialize=lambda modelo, k: Q[k])
+
+
 print("-------------Depositos-----------------")
-for y in range(len(W)):
-  print(W[y]) 
+for y in modelo.w:
+  print(modelo.w[y]) 
 print("-------------Clientes------------------")
-for y in range(len(D)):
-  print(D[y]) 
+for y in modelo.d:
+  print(modelo.d[y]) 
 print("-------------Vehiculos-----------------")
-for y in range(len(Q)):
-  print(Q[y]) 
-
-
+for y in modelo.q:
+  print(modelo.q[y]) 
 
 
 
@@ -94,9 +73,9 @@ def X_ijk_rule(modelo, i):
 modelo.X_ijk = Constraint(modelo.i, rule=X_ijk_rule, doc='Cada cliente j debe ser asignado a un vehiculo K')
 
 
-#def Q_k_rule(modelo, i ):
-# return sum(D[j] for j in modelo.j)*sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) <= Q[k]
-#modelo.Q_k = Constraint(modelo.i, rule=Q_k_rule, doc='Capacidad del conjunto de vehiculos K')
+def Q_k_rule(modelo, i):
+ return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) <= (modelo.q[k] for k in modelo.k)
+modelo.Q_k = Constraint(modelo.i, rule=Q_k_rule, doc='Capacidad del conjunto de vehiculos K')
 
 
 def X_ijk_X_jik_rule(modelo, i):
@@ -107,9 +86,12 @@ def X_ijk__rule(modelo, i):
  return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) <= 1
 modelo.X_ijk__ = Constraint(modelo.i, rule=X_ijk__rule, doc='Garantiza que cada vehiculo atienda almenos una unica ruta')
 
-def W_i_rule(modelo, i ):
- return (D[j] for j in modelo.j)*sum(modelo.z[i,j] for j in modelo.j)  <= W[i] 
-modelo.W_i = Constraint(modelo.i, rule=W_i_rule, doc='Capacidad de los depositos')
+#def W_i_rule(modelo, i , W):
+# return sum(modelo.z[i,j] for j in modelo.j)  <= sum(W[i])
+#modelo.W_i = Constraint(modelo.i, W,  rule=W_i_rule, doc='Capacidad de los depositos i')
+
+#(D[j] for j in modelo.j)*  W[i] 
+
 
 #Funcion Objetivo:
 
