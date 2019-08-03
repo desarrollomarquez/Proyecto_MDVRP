e8610c7cf91c9f2ded65fdc019d3579ddfcc3bd9
Diego Márquez <desarrollo.marquez@gmail.com>
2019-08-03 15:32:36 -0500

    Added workflow prints

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import re
 import pyomo
 from pyomo.environ import *
 from Utils import *
+from time import time #importamos la función time para capturar tiempos
 
 
 # Dominio del Modelo - MDVRP
@@ -73,59 +74,63 @@ modelo.uj = Var(modelo.j, modelo.k, within= NonNegativeReals, bounds=(0.0,None),
 # s.a: - Restricciones:
 
 
-
+t_inicial = time()
 def X_ijk_rule(modelo, i):
  return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) == 1
 modelo.X_ijk = Constraint(modelo.i, rule=X_ijk_rule, doc='Cada cliente j debe ser asignado a un vehiculo K')
 
-print("·······Aplico Restriccion X_ijk ········")
+print("·R1 X_ijk·"," T. Ejecucion sg: ",(time()-t_inicial))
 
+t_inicial = time()
 def Q_k_rule(modelo, i, k):
  return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) <= modelo.q[k]
 modelo.Q_k = Constraint(modelo.i, modelo.k,  rule=Q_k_rule, doc='Capacidad del conjunto de vehiculos K')
 
-print("·······Aplico Restriccion Q_k_rule ········")
+print("·R2 Q_k_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
+t_inicial = time()
 def X_ijk_X_jik_rule(modelo, i):
  return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k ) - sum(modelo.xx[j,i,k] for j in modelo.j for k in modelo.k ) == 0
 modelo.X_ijk_X_jik_rule = Constraint(modelo.i,  rule=X_ijk_X_jik_rule , doc='Conservacion de Flujos')
 
-print("·······Aplico Restriccion X_ijk_X_jik_rule ········")
+print("·R3 X_ijk_X_jik_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
+t_inicial = time()
 def X_ijk__rule(modelo, i):
  return sum(modelo.x[i,j,k] for j in modelo.j for k in modelo.k) <= 1
 modelo.X_ijk__ = Constraint(modelo.i, rule=X_ijk__rule, doc='Garantiza que cada vehiculo atienda almenos una unica ruta')
 
-print("·······Aplico Restriccion X_ijk__rule ········")
+print("·R4 X_ijk__rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
+t_inicial = time()
 def W_i_rule(modelo, i):
  return sum(modelo.d[i]*modelo.z[i,j] for j in modelo.j)  <= modelo.w[i]
 modelo.W_i = Constraint(modelo.i, rule=W_i_rule, doc='Capacidad del conjunto de depositos i')
 
-print("·······Aplico Restriccion W_i_rule ········")
+print("·R5 W_i_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
+t_inicial = time()
 def U_ij_rule(modelo, i, j, k):
  return modelo.ui[i,k] - modelo.uj[j,k] + N*modelo.x[i,j,k] <= N-1
 modelo.U_ij = Constraint(modelo.i, modelo.j, modelo.k, rule=U_ij_rule, doc='Garantiza la eliminacion de SubTours')
 
-print("·······Aplico Restriccion U_ij_rule ········")
+print("·R6 U_ij_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
 # Un cliente puede ser asignado al deposito, unicamente si hay una ruta que parte desde el mismo deposito y transita atravez del cliente.
+t_inicial = time()
 def X_uk_rule(modelo, i, j):
  return  sum(modelo.ux[i,u,k] + modelo.xu[u,j,k] for j in modelo.j for k in modelo.k for u in modelo.u)  - modelo.z[i,j] <= 1 
 modelo.X_uk = Constraint(modelo.i, modelo.j, rule=X_uk_rule, doc='Garantiza asignacion de cliente j si transita por depositos i')
 
-print("·······Aplico Restriccion X_uk_rule ········")
+print(".R7 X_uk_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
 #Funcion Objetivo:
-
+t_inicial = time()
 def objective_rule(modelo):
  return sum(cfv*modelo.x[i,j,k] for i in modelo.i for j in modelo.j for k in modelo.k)
-
-print("·······Aplico F.O objective_rule ········")
- 
 modelo.objective = Objective(rule=objective_rule, sense=minimize, doc='FunciÃ³n objetivo')
 
+print("·F.O objective_rule·"," T. Ejecucion sg: ",(time()-t_inicial))
 
 
 print("·················Finalizo el modelo··························")
